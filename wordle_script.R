292e0211b6999cf9e109c6210ad0c572d221b274
mark-sawyer <alfador_321@hotmail.com>
2022-01-26 13:34:33 +1000

    First commit

---
+++ wordle_script.R
@@ -0,0 +1,109 @@
+
+# Setup ----
+library(tidyverse)
+lengthen_words <- function(word_df) {
+  word_df %>%
+    mutate(
+      `1`=str_sub(word, 1, 1),
+      `2`=str_sub(word, 2, 2),
+      `3`=str_sub(word, 3, 3),
+      `4`=str_sub(word, 4, 4),
+      `5`=str_sub(word, 5, 5)
+    ) %>%
+    pivot_longer(`1`:`5`, names_to="position", values_to="letter") %>%
+    mutate(position=as.integer(position))
+}
+update_remaining_words <- function(guess_word, pattern_found) {
+  if (str_length(guess_word) != 5 | str_length(pattern_found) != 5) {
+    print("incorrect input")
+    return(remaining_words)
+  }
+  
+  lengthen_words(remaining_words) %>%
+    inner_join(
+      tibble(
+        position=1:5,
+        guess_letter=str_split(guess_word, "")[[1]]
+      ),
+      by="position"
+    ) %>%
+    group_by(word) %>%
+    mutate(
+      outcome=case_when(
+        guess_letter == letter ~ "1",
+        guess_letter %in% letter[letter != guess_letter] ~ "2",
+        TRUE ~ "3"
+      )
+    ) %>%
+    summarise(pattern=str_c(outcome, collapse="")) %>%
+    filter(pattern == pattern_found) %>%
+    select(-pattern)
+}
+get_best <- function() {
+  if (pull(count(words)) == pull(count(remaining_words))) {
+    return(
+      read.csv("get_best_first_turn.csv") %>%
+        as_tibble() %>%
+        select(-X) %>%
+        mutate(remaining=TRUE) %>%
+        filter(row_number() <= 10)
+    )
+  }
+  
+  lengthen_words(remaining_words) %>%
+    inner_join(
+      lengthen_words(words) %>%
+        rename(guess="word", guess_letter="letter"),
+      by="position"
+    ) %>%
+    group_by(guess, word) %>%
+    mutate(
+      outcome=case_when(
+        guess_letter == letter ~ "1",
+        guess_letter %in% letter[letter != guess_letter] ~ "2",
+        TRUE ~ "3"
+      )
+    ) %>%
+    summarise(pattern=str_c(outcome, collapse="")) %>%
+    group_by(guess, pattern) %>%
+    summarise(n=n()) %>%
+    mutate(prob=n / sum(n)) %>%
+    summarise(
+      expectation=sum(n * prob),
+      max=max(n)
+    ) %>%
+    mutate(remaining=guess %in% remaining_words[["word"]]) %>%
+    arrange(expectation, desc(remaining)) %>%
+    filter(row_number() <= 10)
+}
+
+words <- read.delim("wordle-answers-alphabetical.txt", header=FALSE) %>%
+  as_tibble() %>%
+  rename(word="V1")
+
+remaining_words <- words
+
+# Play ----
+# 1
+get_best()
+remaining_words <- update_remaining_words("", "")
+
+# 2
+get_best()
+remaining_words <- update_remaining_words("", "")
+
+# 3
+get_best()
+remaining_words <- update_remaining_words("", "")
+
+# 4
+get_best()
+remaining_words <- update_remaining_words("", "")
+
+# 5
+get_best()
+remaining_words <- update_remaining_words("", "")
+
+# 6
+get_best()
+remaining_words <- update_remaining_words("", "")
